aecb3c4e20961f76a10df6b56c5145ae22e0dd2e
randy <randyr_1993@yahoo.com>
2022-07-30 19:33:53 -0400

    Created tests, implemented logic, and tested logic

--- src/main/java/models/Sneaker.java
+++ src/main/java/models/Sneaker.java
@@ -8,4 +8,62 @@ public class Sneaker {
     private int size;
     private int qty;
     private float price;
+
+    public void setName(String name) {
+        this.name = name;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setID(int id) {
+        this.id = id;
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setSport(String sport) {
+        this.sport = sport;
+    }
+
+    public String getSport() {
+        return sport;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public int getQty(){
+        return qty;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
 }
--- src/test/java/models/SneakerTest.java
+++ src/test/java/models/SneakerTest.java
@@ -1,4 +1,91 @@
 package models;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class SneakerTest {
+
+    @Test
+    public void setNameTest() {
+        //given (1)
+        //This is our test data that we will compare to the actual values returned from the object.
+        String expected = "Air Max";
+
+        //when (2)
+        //We create a new Sneaker object for testing. Then we invoke setName() passing in the test data.
+        Sneaker testSneaker = new Sneaker();
+        testSneaker.setName(expected);
+
+        //then (3)
+        //Retrieve the objects 'name' value with getName() and compare with the value that is expected.
+        Assertions.assertEquals(expected, testSneaker.getName());
+    }
+    @Test
+    public void setIDTest() {
+
+        int expected = 28;
+
+        Sneaker testSneaker = new Sneaker();
+        testSneaker.setID(expected);
+
+        Assertions.assertEquals(expected, testSneaker.getID());
+
+    }
+
+    @Test
+    public void setBrandTest() {
+
+        String expected = "Nike";
+
+        Sneaker testSneaker = new Sneaker();
+        testSneaker.setBrand(expected);
+
+        Assertions.assertEquals(expected, testSneaker.getBrand());
+
+    }
+
+    @Test
+    public void setSportTest() {
+
+        String expected = "Running";
+
+        Sneaker testSneaker = new Sneaker();
+        testSneaker.setSport(expected);
+
+        Assertions.assertEquals(expected, testSneaker.getSport());
+    }
+    @Test
+    public void setSize() {
+
+        int expected = 9;
+
+        Sneaker testSneaker = new Sneaker();
+        testSneaker.setSize(expected);
+
+        Assertions.assertEquals(expected, testSneaker.getSize());
+    }
+
+    @Test
+    public void setQty() {
+
+        int expected = 2;
+
+        Sneaker testSneaker = new Sneaker();
+        testSneaker.setQty(expected);
+
+        Assertions.assertEquals(expected, testSneaker.getQty());
+    }
+
+    @Test
+    public void setPrice() {
+
+        float expected = 199;
+
+        Sneaker testSneaker = new Sneaker();
+        testSneaker.setPrice(expected);
+
+        Assertions.assertEquals(expected, testSneaker.getPrice());
+    }
+
+
 }
